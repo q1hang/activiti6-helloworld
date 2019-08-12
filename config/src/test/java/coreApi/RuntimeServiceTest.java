4af0ee7afcfea5eb947591e30bc6e42e163c0a45
q1hang <1399841000@qq.com>
2019-08-12 10:28:38 +0800

    0

--- config/src/test/java/coreApi/RuntimeServiceTest.java
+++ config/src/test/java/coreApi/RuntimeServiceTest.java
@@ -1,17 +1,10 @@
 package coreApi;
 
 import com.google.common.collect.Maps;
-import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
-import org.activiti.engine.repository.Deployment;
-import org.activiti.engine.repository.DeploymentBuilder;
-import org.activiti.engine.repository.DeploymentQuery;
-import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.runtime.Execution;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.runtime.ProcessInstanceBuilder;
-import org.activiti.engine.task.IdentityLink;
-import org.activiti.engine.task.Task;
 import org.activiti.engine.test.ActivitiRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -30,51 +23,51 @@ public class RuntimeServiceTest {
 
     @Test
     @org.activiti.engine.test.Deployment(resources = {"my-process.bpmn20.xml"})
-    public void testStartProcess(){
+    public void testStartProcess() {
         RuntimeService runtimeService = activitiRule.getRuntimeService();
-        Map<String,Object> variables= Maps.newHashMap();
-        variables.put("key1","value1");
+        Map<String, Object> variables = Maps.newHashMap();
+        variables.put("key1", "value1");
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("my-process", variables);
-        LOGGER.info("processInstance= {}",processInstance);
+        LOGGER.info("processInstance= {}", processInstance);
     }
 
     @Test
     @org.activiti.engine.test.Deployment(resources = {"my-process.bpmn20.xml"})
-    public void testProcessInstanceBuilder(){
+    public void testProcessInstanceBuilder() {
         RuntimeService runtimeService = activitiRule.getRuntimeService();
-        Map<String,Object> variables= Maps.newHashMap();
-        variables.put("key1","value1");
+        Map<String, Object> variables = Maps.newHashMap();
+        variables.put("key1", "value1");
         ProcessInstanceBuilder processInstanceBuilder = runtimeService.createProcessInstanceBuilder();
         ProcessInstance processInstance = processInstanceBuilder.businessKey("businessKey001").processDefinitionKey("my-process")
                 .variables(variables).start();
 
 //        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("my-process", variables);
-        LOGGER.info("processInstance= {}",processInstance);
+        LOGGER.info("processInstance= {}", processInstance);
     }
 
     @Test
     @org.activiti.engine.test.Deployment(resources = {"my-process.bpmn20.xml"})
-    public void testVariables(){
+    public void testVariables() {
         RuntimeService runtimeService = activitiRule.getRuntimeService();
-        Map<String,Object> variables= Maps.newHashMap();
-        variables.put("key1","value1");
-        variables.put("key2","value2");
-        variables.put("key3","value3");
+        Map<String, Object> variables = Maps.newHashMap();
+        variables.put("key1", "value1");
+        variables.put("key2", "value2");
+        variables.put("key3", "value3");
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("my-process", variables);
-        LOGGER.info("processInstance= {}",processInstance);
-        runtimeService.setVariable(processInstance.getId(),"key3","v33333");
-        runtimeService.setVariable(processInstance.getId(),"key4","value4");
+        LOGGER.info("processInstance= {}", processInstance);
+        runtimeService.setVariable(processInstance.getId(), "key3", "v33333");
+        runtimeService.setVariable(processInstance.getId(), "key4", "value4");
 
         Map<String, Object> variables1 = runtimeService.getVariables(processInstance.getId());
-        LOGGER.info("Variable1 = {}",variables1);
+        LOGGER.info("Variable1 = {}", variables1);
     }
 
     @Test
     @org.activiti.engine.test.Deployment(resources = {"my-process.bpmn20.xml"})
-    public void testProcessInstanceQuery(){
+    public void testProcessInstanceQuery() {
         RuntimeService runtimeService = activitiRule.getRuntimeService();
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("my-process");
-        LOGGER.info("processInstance= {}",processInstance);
+        LOGGER.info("processInstance= {}", processInstance);
 
         ProcessInstance processInstance1 = runtimeService.createProcessInstanceQuery()
                 .processInstanceId(processInstance.getId()).singleResult();
@@ -82,24 +75,45 @@ public class RuntimeServiceTest {
 
     @Test
     @org.activiti.engine.test.Deployment(resources = {"my-process.bpmn20.xml"})
-    public void testExecutionQuery(){
+    public void testExecutionQuery() {
         RuntimeService runtimeService = activitiRule.getRuntimeService();
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("my-process");
-        LOGGER.info("processInstance= {}",processInstance);
+        LOGGER.info("processInstance= {}", processInstance);
 
         List<Execution> executions = runtimeService.createExecutionQuery().listPage(0, 100);
-        executions.forEach(x->LOGGER.info("execution = {}",x));
+        executions.forEach(x -> LOGGER.info("execution = {}", x));
     }
 
     @Test
-    @org.activiti.engine.test.Deployment(resources = {"my-process_trigger.bpmn20.xml"})
-    public void testTrigger(){
+    @org.activiti.engine.test.Deployment(resources = {"my-process-trigger.bpmn20.xml"})
+    public void testTrigger() {
         RuntimeService runtimeService = activitiRule.getRuntimeService();
-        runtimeService.startProcessInstanceByKey("my-process_trigger");
+        runtimeService.startProcessInstanceByKey("my-process-trigger");
 
+        Execution execution = runtimeService.createExecutionQuery().activityId("someTask").singleResult();
 
-     //   runtimeService.create
+        LOGGER.info("execution = {}", execution);
+        runtimeService.trigger(execution.getId());
+        execution = runtimeService.createExecutionQuery().activityId("someTask").singleResult();
+        LOGGER.info("execution = {}", execution);
+    }
 
+    @Test
+    @org.activiti.engine.test.Deployment(resources = {"my-process-signal-received.bpmn20.xml"})
+    public void testSignalEventReceived(){
+        RuntimeService runtimeService = activitiRule.getRuntimeService();
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("my-process-signal-received");
 
+        Execution execution = runtimeService.createExecutionQuery()
+                .signalEventSubscriptionName("my-signal").singleResult();
+
+        LOGGER.info("execution = {}",execution);
+
+        runtimeService.signalEventReceived("my-signal");
+
+        execution = runtimeService.createExecutionQuery()
+                .signalEventSubscriptionName("my-signal").singleResult();
+
+        LOGGER.info("execution = {}",execution);
     }
 }
